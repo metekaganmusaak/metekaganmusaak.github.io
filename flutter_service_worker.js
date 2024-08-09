292cfd46a25c0495c3eb4e8c8eb7e5f491ba7677
metekaganmusaak <metemusaak@gmail.com>
2024-08-09 18:21:15 +0300

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -53,7 +53,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "e7791b15d28669bcd309335cd382d624
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "69bd5134329ee51e9c12c65ff8776453",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "0c779565d75c44a44f1653ef4df76192",
+"flutter_bootstrap.js": "68c71ad051740e883e3d035370c6ee67",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
